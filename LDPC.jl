91e97e93401ec55618b6bf1bc3705cb0ac71d924
AllanEduardo42 <67806687+AllanEduardo42@users.noreply.github.com>
2025-02-05 07:35:35 -0300

    resultados RBP

--- LDPC.jl
+++ LDPC.jl
@@ -48,11 +48,11 @@ STOP::Bool = false # stop simulation at zero syndrome (if true, BER curves are
 ################################## 5) NUMBERS ##################################
 
 MAX::Int = 50
-MAXRBP::Int = 10
+MAXRBP::Int = 5
 DECAY::Float64 = 0.8
 SNR = collect(0.8:0.4:2.0)
 SNRTEST = [1.6]
-TRIALS = 256*[1, 10, 100, 1000, 10000]
+TRIALS = 512*[1, 10, 100, 1000, 10000]
 TRIALSTEST = [1]
 
 ################################ 6) BP SCHEDULE ################################
@@ -70,12 +70,12 @@ Maxiters = zeros(Int,7)
 Decays = Vector{Union{Vector{<:AbstractFloat},Nothing}}(nothing,6)
 
 #Flooding
-Modes[1] = 0
+Modes[1] = 1
 Bptypes[1] = "FAST"
 Maxiters[1] = MAX
 
 #LBP
-Modes[2] = 0
+Modes[2] = 1
 Bptypes[2] = "FAST"
 Maxiters[2] = MAX
 
@@ -85,7 +85,7 @@ Bptypes[3] = "FAST"
 Maxiters[3] = MAX
 
 #RBP
-Modes[4] = 0
+Modes[4] = 1
 Bptypes[4] = "FAST"
 Maxiters[4] = MAXRBP
 Decays[4] = [DECAY]
@@ -98,7 +98,7 @@ Maxiters[5] = MAXRBP
 Decays[5] = [DECAY]
 
 #List-RBP
-Modes[6] = 0
+Modes[6] = 1
 Bptypes[6] = "FAST"
 Maxiters[6] = MAXRBP
 Decays[6] = [DECAY]
