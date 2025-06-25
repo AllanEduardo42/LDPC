5059f43086d385277f98a9bade676856ea69120b
AllanEduardo42 <67806687+AllanEduardo42@users.noreply.github.com>
2025-06-25 12:15:19 -0300

    simulações 2

---
+++ Saved Data/Testes NR5G 528 1|3 2.5/plot.jl
@@ -0,0 +1,16 @@
+N = 528
+R = [1,3]
+iters = 256000
+maxiter = 50
+EbNo = 2.5
+protocol = "NR5G"
+decays = [0.7, 0.8, 0.9, 1.0]
+
+best = [1,1,1,1,2,1,2]
+
+onlybest = true
+
+include("../plot_tests.jl")
+
+
+
