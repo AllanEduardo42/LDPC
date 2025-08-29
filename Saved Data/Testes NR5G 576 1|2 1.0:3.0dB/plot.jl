673ffe981804ca2f8ce5fa4534ad508f6e31030e
AllanEduardo42 <67806687+AllanEduardo42@users.noreply.github.com>
2025-08-29 16:39:06 -0300

    new figures to paper

--- Saved Data/Testes NR5G 576 1|2 1.0:3.0dB/plot.jl
+++ Saved Data/Testes NR5G 576 1|2 1.0:3.0dB/plot.jl
@@ -22,7 +22,7 @@ gr()
 
 FB = ["F","B"]
 # markers = [:none, :none, :dtriangle, :circle, :rect, :utriangle, :diamond, :cross, :star5, :hexagon]
-modes_markers_lines =  ["Flooding"             :none            :solid          
+modes_markers_lines_lines =  ["Flooding"             :none            :solid          
                         "LBP"                  :none            :solid
                         "RBP"                  :dtriangle       :solid
                         "RD-RBP 0.85"          :utriangle       :solid                
@@ -38,8 +38,8 @@ directory = "./Saved Data/Testes NR5G 576 1|2 1.0:3.0dB/"
 # liminf = -5
 # limsup = 0
 
-FER_EbN0 = zeros(length(EbN0),size(modes_markers_lines,1))
-BER_EbN0 = zeros(length(EbN0),size(modes_markers_lines,1))
+FER_EbN0 = zeros(length(EbN0),size(modes_markers_lines_lines,1))
+BER_EbN0 = zeros(length(EbN0),size(modes_markers_lines_lines,1))
 
 p1 = plot()
 p2 = plot()
@@ -52,8 +52,8 @@ for j=1:2
         if active[k] == 1
             title = FB[j]*"ER $protocol (N = $N, R = $(R[1])/$(R[2]), Eb/N0 = $(EbN0[k])dB)"
             p = plot()
-            for i in axes(modes_markers_lines,1)
-                str = modes_markers_lines[i,1]
+            for i in axes(modes_markers_lines_lines,1)
+                str = modes_markers_lines_lines[i,1]
                 x = readdlm(directory*FB[j]*"ER_"*str*".txt",'\t',Float64,'\n')
                 x = x[:,k]
                 if j == 1
@@ -66,9 +66,9 @@ for j=1:2
                     # log10.(x),
                     x[1:maxiter],
                     ylabel=FB[j]*"ER",
-                    label=modes_markers_lines[i,1],
+                    label=modes_markers_lines_lines[i,1],
                     lw=3,
-                    ls=modes_markers_lines[i,3],
+                    ls=modes_markers_lines_lines[i,3],
                     # title=title,
                     ylims=(liminf,limsup),
                     # xlim=(1,maxiter),
@@ -85,7 +85,7 @@ for j=1:2
                     legend_font_pointsize = 10,
                     # legend = :outertopright,
                     size = 1.5 .*(600,500),
-                    markershape = modes_markers_lines[i,2],
+                    markershape = modes_markers_lines_lines[i,2],
                     left_margin=3Plots.mm,
                     bottom_margin=-3Plots.mm,
                     top_margin=3Plots.mm,
@@ -111,32 +111,48 @@ p = plot(p1,p2,layout=(2,1),bottom_margin=-1Plots.mm,)
 display(p)
 Plots.pdf(p,directory*"/FER_BER_576")
 
-# colors = [1, 2, 4, 5, 6, 18, 7]
+colors = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # # FER x EbN0
-# PLOT = plot(
-#     EbN0,FER_EbN0,
-#     minorgrid=true,
-#     minorgridalpha=0.05,
-#     yscale=:log10,
-#     ylabel="FER",
-#     xlabel="EbN0 (dB)",
-#     ylims=(10^(-5),1),
-#     color=colors',
-#     label=permutedims(modes_markers_lines[:,1]),
-#     markershape=permutedims(modes_markers_lines[:,2]),
-#     lw=2.5,
-#     markersize=5,
-#     markerstrokewidth = 0.25,
-#     guidefontsize=15,
-#     tickfontsize=12,
-#     legend_font_pointsize = 10,
-#     fontfamily="Computer Modern",
-#     size = 1.5 .*(600,400),
-#     left_margin=3Plots.mm,
-#     framestyle=:box
-# )
-# display(PLOT)
+for j = 1:2
+    for k in eachindex(EbN0)
+        for i in axes(modes_markers_lines_lines,1)
+            str = modes_markers_lines_lines[i,1]
+            x = readdlm(directory*FB[j]*"ER_"*str*".txt",'\t',Float64,'\n')
+            x = x[:,k]
+            if j == 1
+                FER_EbN0[k,i] = x[iter]
+            else
+                BER_EbN0[k,i] = x[iter]
+            end
+        end
+    end
+end
+
+PLOT = plot(
+    EbN0,FER_EbN0,
+    minorgrid=true,
+    minorgridalpha=0.05,
+    yscale=:log10,
+    ylabel="FER",
+    xlabel=L"E_b/N_0"*" (dB)",
+    ylims=(10^(-5),1),
+    color=colors',
+    label=permutedims(modes_markers_lines_lines[:,1]),
+    markershape=permutedims(modes_markers_lines_lines[:,2]),
+    line=permutedims(modes_markers_lines_lines[:,3]),
+    lw=2.5,
+    markersize=5,
+    markerstrokewidth = 0.25,
+    guidefontsize=15,
+    tickfontsize=12,
+    legend_font_pointsize = 10,
+    fontfamily="Computer Modern",
+    size = 1.5 .*(600,400),
+    left_margin=3Plots.mm,
+    framestyle=:box
+)
+display(PLOT)
 # BER x EbN0
 # PLOT = plot(
 #     EbN0,BER_EbN0,
@@ -144,15 +160,65 @@ Plots.pdf(p,directory*"/FER_BER_576")
 #     yscale=:log10,
 #     xlabel="EbN0 (dB)",
 #     ylims=(10^(-8),0.1),
-#     label=permutedims(modes_markers_lines[:,1]),
-#     markershape=permutedims(modes_markers_lines[:,2]),
+#     label=permutedims(modes_markers_lines_lines[:,1]),
+#     markershape=permutedims(modes_markers_lines_lines[:,2]),
 #     lw=2,
 #     # title="BER x EbN0 (Iter = 10, N = $N, R = $(R[1])/$(R[2]))",
 #     size = 1.5 .*(600,400),
 #     # ylims=(-6,-1)
 # )
 # display(PLOT)
-# Plots.pdf(PLOT,directory*"/FER_dB.pdf")
+Plots.pdf(PLOT,directory*"/FER_dB.pdf")
+
+### C&DR-RBP
+
+p = plot()
+liminf = 10^(-3)
+limsup = 1
+
+for k in axes(modes_markers_lines,1)
+    str = modes_markers_lines[k,1]
+    labels = modes_markers_lines[k,1]
+    x = readdlm(directory*"FER_"*str*".txt",'\t',Float64,'\n')
+    x = x[1:8,4]
+    line = :solid
+    # labels = permutedims(labels)
+    p = plot!(
+        1:8,
+        x,
+        ylabel="FER",
+        label=labels,
+        lw=3,
+        ls=modes_markers_lines[k,3],
+        # title=title,
+        ylims=(liminf,limsup),
+        # xlim=(1,maxiter),
+        minorgrid=true,
+        minorgridalpha=0.05,
+        yscale=:log10,
+        color=colors[k],
+        markersize=5,
+        markerstrokewidth = 0.25,
+        guidefontsize=15,
+        tickfontsize=12,
+        fontfamily="Computer Modern",
+        xlabel="Iteration",
+        # legend_title = "(Algorithm, best decay factor)",
+        # legend_title_font_pointsize = 10,
+        legend_font_pointsize = 10,
+        # legend = :outertopright,
+        size = 1.5 .*(600,300),
+        markershape = modes_markers_lines[k,2],
+        left_margin=5Plots.mm,
+        bottom_margin=5Plots.mm,
+        top_margin=3Plots.mm,
+        framestyle=:box
+    )
+end
+
+# p = plot(p1,p2,layout=(2,1),fontfamily="Computer Modern",xlabel="Iteration",bottom_margin=-1Plots.mm,)
+display(p)
+Plots.pdf(p,directory*"/FER_BER_C&DR_576")
 
 
 
